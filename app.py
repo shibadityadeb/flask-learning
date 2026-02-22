6048a5136a173d3a4bd69fbd625f97fcf35e3722
Shibaditya Deb <shibadityadeb.official@gmail.com>
2026-02-22 16:06:39 +0530

    feat: update implementation

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+#flask app routing
+
 from flask import Flask
 #create a simple application
 app = Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def welcome():
+    return "welcome to my flask app"
+
+@app.route("/index",methods=["GET"])
+def index():
+    return "welcome to the index page"
+#variable routing
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person has passed and the score is "+ str(score)
+
+if __name__=="__main__":
+    app.run(debug=True)
